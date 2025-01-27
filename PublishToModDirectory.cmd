d263fa2047571c87b7c9055a1c9118cab10ef647
Will <125041335+will258012@users.noreply.github.com>
2025-01-27 20:31:19 +0800

    设置标题、启用静默

--- PublishToModDirectory.cmd
+++ PublishToModDirectory.cmd
@@ -1,5 +1,7 @@
-dotnet clean -nologo
+@echo off
+title Mod Uploader - Publish
+dotnet clean -nologo -v q
 rd /s /q "%localappdata%\Colossal Order\Cities_Skylines\Addons\Mods\ModUploader"
-dotnet publish ModUploader.App/ModUploader.App.csproj  /p:Configuration=Release -nologo
+dotnet publish ModUploader.App/ModUploader.App.csproj  /p:Configuration=Release -nologo -v q
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
-msbuild ModUploader.Mod/ModUploader.Mod.csproj /p:Configuration=Release -nologo
+msbuild ModUploader.Mod/ModUploader.Mod.csproj /p:Configuration=Release -nologo -v:q
